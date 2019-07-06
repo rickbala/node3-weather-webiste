397aace0ed60fe58dca76b377e7c099bc672deb0
Ricardo <ricardovbalarini@gmail.com>
2019-07-06 12:50:53 -0300

    remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("js file loaded from public/js")
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
